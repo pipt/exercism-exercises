8aff888f1c66a0f33525fc900ec96e3c19611749
Pip Taylor <pip@evilgeek.co.uk>
2013-12-24 16:07:39 +0000

    Extract functionality in to methods.
    Introduced prune_identical_words and anagram? methods.

--- anagram/anagram.rb
+++ anagram/anagram.rb
@@ -4,13 +4,17 @@ class Anagram
   end
 
   def match(words)
-    words.
-      delete_if { |possible_match| possible_match.downcase == word }.
-      select { |possible_match|
-        possible_match.downcase.chars.sort == word.chars.sort
-      }
+    prune_identical_words(words).select(&method(:anagram?))
   end
 
 private
   attr_reader :word
+
+  def prune_identical_words(words)
+    words.delete_if { |possible_match| possible_match.downcase == word }
+  end
+
+  def anagram?(possible_match)
+    possible_match.downcase.chars.sort == word.chars.sort
+  end
 end

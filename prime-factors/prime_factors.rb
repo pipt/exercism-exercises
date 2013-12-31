740b7fe3d1450f2e64b2398fbfe9cbbbce265290
Pip Taylor <pip@evilgeek.co.uk>
2013-12-31 11:24:27 +0000

    Improved (now recursive) prime factors program.

--- prime-factors/prime_factors.rb
+++ prime-factors/prime_factors.rb
@@ -2,13 +2,8 @@ require "prime"
 
 class PrimeFactors
   def self.for(n)
-    remaining = n
-    Prime.each_with_object([]) { |prime, prime_factors|
-      break prime_factors if remaining == 1
-      while remaining % prime == 0
-        prime_factors << prime
-        remaining /= prime
-      end
-    }
+    return [] if n == 1
+    first_prime_factor = Prime.find { |prime| n % prime == 0 }
+    [first_prime_factor] + self.for(n / first_prime_factor)
   end
 end
